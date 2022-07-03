0152d31d706e0bce811077a28d61f8c594d32e12
moraghan <peter.moraghan@me.com>
2022-07-03 10:40:42 +0100

    Add path to config file

--- helper.py
+++ helper.py
@@ -1,15 +1,19 @@
 from omegaconf import OmegaConf
+import os
 
-config_file = 'config.yaml'
+config_file = os.path.join(os.path.dirname(__file__), 'config.yaml')
 
-def get_db_connection()->str :
+
+def get_db_connection() -> str:
     conf = OmegaConf.load(config_file)
     return conf.DATABASE.DATABASE_URL
 
-def get_api_key()->str :
+
+def get_api_key() -> str:
     conf = OmegaConf.load(config_file)
     return conf.APP.API_KEY
 
-def get_request_types()->dict:
+
+def get_request_types() -> dict:
     conf = OmegaConf.load(config_file)
     return conf.TMDB_REQUEST_TYPES
